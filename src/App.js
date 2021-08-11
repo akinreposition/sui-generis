b3b8880dd03d36dbe2ac9918ac67d61292df2055
akinrepostion <odekuakinlolu@gmail.com>
2021-08-11 13:02:34 +0100

    first commit with  pictures

---
+++ src/App.js
@@ -0,0 +1,14 @@
+ 
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello Sui Generis!!!!</h1>
+      <p>I love semicolon.</p>
+    </div>
+    
+  );
+}
+
+export default App;
