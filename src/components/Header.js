4f3777914b7619547b85df0b13f1fbcf532b219a
akinrepostion <odekuakinlolu@gmail.com>
2021-09-01 13:57:22 +0100

    header 1st fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,19 @@
 import React from "react";
 
 const Header = () => {
-  
+  const { welcome } = headerText;
   return (
     <div className="f1 tc mt-7">
       <h1 id="h1">Hello Natives!</h1>
       <p>
-        Welcome Ancestors! search
-        for natives and contact details.
+        {welcome}
       </p>
     </div>
   );
 };
 
 const headerText = {
-
+  welcome : "Welcome Ancestors! search" +
+              "for natives and contact details."
 }
 export default Header;
